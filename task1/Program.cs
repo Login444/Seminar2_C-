dee2dc6528f07bdc8effc8df684f2c72e0611428
Artemiy <birukovartemij@gmail.com>
2022-12-18 10:35:29 +0300

    task1, ищем наибольшую из цифр

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int number = new Random().Next(10,100);
+Console.WriteLine(number);
+
+if (number/10 > number%10)
+{
+    Console.WriteLine(number/10);  // Строгое деление на 10 (без остатка) даст нам "десятки"
+}
+else
+{
+    Console.WriteLine(number%10); // Находим остаток от деления на 10, т.е. "единицы"
+}
