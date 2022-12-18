1f404755042d1e2958355541cb6b2c7e98b00a61
Artemiy <birukovartemij@gmail.com>
2022-12-18 10:37:59 +0300

    task1, нашли наибольшую из цифр

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,4 +1,4 @@
-﻿int number = new Random().Next(10,100);
+﻿int number = new Random().Next(10,100); // в задании от 10 до 99, но что бы 99 попала надо поставить границу - 100
 Console.WriteLine(number);
 
 if (number/10 > number%10)
